96bbe664fe02ad2a146abfc52f1dc6f39391b969
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-08-12 13:41:51 +0300

    eslint8

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,10 @@ export default [
 
   // Global settings
   {
+    ignores: [
+      'lib/generated/', 
+      
+    ],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
